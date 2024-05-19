0fb2d8600ae656059b28bcb35ccc2cf83e014f0c
Marcus-Schebek <marcusschebek@gmail.com>
2024-05-19 15:46:53 -0300

    Sidebar no Index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Map  from "./components/Map/Map";
+import Sidebar from './components/Sidebar/Sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Sidebar></Sidebar>
     <Map />
   </React.StrictMode>
 );
