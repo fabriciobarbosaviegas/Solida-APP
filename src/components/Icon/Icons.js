a57261d1e91a01e1451a708cb91da58ccf9b328f
Marcus-Schebek <marcusschebek@gmail.com>
2024-05-19 19:56:04 -0300

    Sidebar com WaringAtivo

--- src/components/Icon/Icons.js
+++ src/components/Icon/Icons.js
@@ -2,11 +2,11 @@
 import React from 'react';
 import { Box, Image, Text, useBreakpointValue } from '@chakra-ui/react';
 
-const Icon = ({ ImgSrc, text }) => {
+const Icon = ({ ImgSrc, text, onClick }) => {
   const size = useBreakpointValue({ base: "1.75rem", sm: "1rem", md: "1.75rem", lg: "3rem" });
 
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" m="0.25rem auto 0.75rem">
+    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" m="0.25rem auto 0.75rem" onClick={onClick}>
       <Image src={ImgSrc} alt={text} boxSize={size} />
       <Text textAlign="center" fontSize="12px" minH="20px">{text}</Text>
     </Box>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,43 +1,59 @@
-import React from 'react';
-import { Box, Spacer, useBreakpointValue } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Box, Spacer, useBreakpointValue, Flex } from '@chakra-ui/react';
+import WarningCard from '../WarningCard/WarningCard';
 import WarningIcon from '../../assets/DenunciaIcon.svg'
 import MapIcon from '../../assets/MapIcon.svg'
 import UserIcon from '../../assets/UserIcon.svg'
 import Icon from '../Icon/Icons'
+import Geleira from '../../assets/Geleira.png'
+
+const SidebarBox = ({children, ...props}) => (
+  <Box
+    position="fixed"
+    left={0}
+    bottom={0}
+    height={{ base: "10%", md: "100%" }}
+    width={{ base: "100%", md: "8%" }}
+    display="flex"
+    padding={4}
+    boxSizing="border-box"
+    zIndex={5}
+    backgroundColor="white"
+    {...props}
+  >
+    {children}
+  </Box>
+);
 
 const Sidebar = () => {
+  const [showWarningCard, setShowWarningCard] = useState(false);
   const direction = useBreakpointValue({ base: "row", md: "column" });
   const align = useBreakpointValue({ base: "center", md: "stretch" });
-  const top = useBreakpointValue({ base: "auto", md:"0"});
+  const top = useBreakpointValue({ base: "auto", md: "0" });
   const displaySpacer = useBreakpointValue({ base: "none", md: "block" });
   const iconWidth = useBreakpointValue({ base: "48px", md: "73px" });
   const iconHeight = useBreakpointValue({ base: "58px", md: "73px" });
 
   return (
-    <Box
-      position="fixed"
-      left={0}
-      top={top}
-      bottom={0}
-      height={{ base: "10%", md: "100%" }}
-      width={{ base: "100%", md: "9%" }}
-      display="flex"
-      flexDirection={direction}
-      alignItems={align}
-      justifyContent={'space-between'}
-      padding={4}
-      boxSizing="border-box"
-      zIndex={1}
-      backgroundColor="white"
-      align={align}
-      
-    >
-        <Icon ImgSrc={MapIcon} text={"Mapa"} width={iconWidth} height={iconHeight}/> 
-        <Icon ImgSrc={WarningIcon} text={"Denuncias"} width={iconWidth} height={iconHeight}/> 
-      <Spacer display={displaySpacer}/>
-        <Icon ImgSrc={UserIcon} text={"Você"} width={iconWidth} height={iconHeight}/> 
-
-    </Box>
+    <Flex direction={{ base: "column-reverse", md: "row" }}>
+      <SidebarBox
+        top={top}
+        flexDirection={direction}
+        alignItems={align}
+        justifyContent={'space-between'}
+        align={align}
+      >
+        <Icon ImgSrc={MapIcon} text={"Mapa"} width={iconWidth} height={iconHeight} />
+        <Icon ImgSrc={WarningIcon} text={"Denuncias"} width={iconWidth} height={iconHeight} onClick={() => setShowWarningCard(!showWarningCard)} />
+        <Spacer display={displaySpacer} />
+        <Icon ImgSrc={UserIcon} text={"Você"} width={iconWidth} height={iconHeight} />
+      </SidebarBox>
+      {showWarningCard &&
+        <SidebarBox position="fixed" left={{ base: "0", md: "8%" }} bottom={{ base: "0", md: "0" }} width={{ base: "100%", md: "35%" }} height={{ base: "100%", md: "auto" }} zIndex={1}>
+          <WarningCard title="Netto precisa de ajuda" ImgSrc={Geleira} text="Ola amiguinhos. Gelo Gelo Gelo Gelo Gelo, Geleira Geleira Geleira. Escalada Escalada Escalada Escalada Escalada Escalala. " />
+        </SidebarBox>
+      }
+    </Flex>
   );
 };
 
