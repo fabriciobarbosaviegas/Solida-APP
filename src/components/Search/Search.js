626fc269cbee0d8e18fc04c21808254487495ae7
Fabrício Barbosa Viegas <incy8498@gmail.com>
2024-05-19 23:04:56 -0300

    Barra de pesquisa

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
-import { Input } from '@chakra-ui/react';
+import { Input, InputGroup, InputRightAddon, Button } from '@chakra-ui/react';
+import { Search2Icon } from "@chakra-ui/icons";
 
 const Search = ({ onLoad, onPlaceChanged, size }) => (
   <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-    <Input type="text" placeholder="Pesquisar lugar..." size={size} borderRadius="2xl" bg="white" boxShadow='2xl' />
+    <InputGroup borderRadius="2xl" size={size}>
+    <Input type="text" placeholder="Pesquisar lugar..." size={size} borderRadius="2xl" bg="white" />
+    <InputRightAddon p={0} border="none" borderRadius="2xl">
+      <Button size="sm" borderLeftRadius={0} borderRightRadius="2xl" bg="white"><Search2Icon color="gray.600" /></Button>
+    </InputRightAddon>
+    </InputGroup>
   </Autocomplete>
 );
 
