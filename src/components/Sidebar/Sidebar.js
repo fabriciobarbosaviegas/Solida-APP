cf8c34e230815ef1dc3556876f0c911fe8c16dc9
Marcus-Schebek <marcusschebek@gmail.com>
2024-05-19 21:04:38 -0300

    Adicionei um Drawer ao Profile e ao Warning

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { Box, Spacer, useBreakpointValue, Flex } from '@chakra-ui/react';
+import React from 'react';
+import { Box, Spacer, useBreakpointValue, Flex, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, useDisclosure } from '@chakra-ui/react';
 import WarningCard from '../WarningCard/WarningCard';
+import ProfileCard from '../ProfileCard/ProfileCard';
 import WarningIcon from '../../assets/DenunciaIcon.svg'
 import MapIcon from '../../assets/MapIcon.svg'
 import UserIcon from '../../assets/UserIcon.svg'
 import Icon from '../Icon/Icons'
 import Geleira from '../../assets/Geleira.png'
 
-const SidebarBox = ({children, ...props}) => (
+const SidebarBox = ({ children, ...props }) => (
   <Box
     position="fixed"
     left={0}
@@ -26,13 +27,15 @@ const SidebarBox = ({children, ...props}) => (
 );
 
 const Sidebar = () => {
-  const [showWarningCard, setShowWarningCard] = useState(false);
   const direction = useBreakpointValue({ base: "row", md: "column" });
   const align = useBreakpointValue({ base: "center", md: "stretch" });
   const top = useBreakpointValue({ base: "auto", md: "0" });
   const displaySpacer = useBreakpointValue({ base: "none", md: "block" });
+  const drawerSize = useBreakpointValue({ base: "full", md: "md" });
   const iconWidth = useBreakpointValue({ base: "48px", md: "73px" });
   const iconHeight = useBreakpointValue({ base: "58px", md: "73px" });
+  const { isOpen: isWarningOpen, onOpen: onWarningOpen, onClose: onWarningClose } = useDisclosure();
+  const { isOpen: isProfileOpen, onOpen: onProfileOpen, onClose: onProfileClose } = useDisclosure();
 
   return (
     <Flex direction={{ base: "column-reverse", md: "row" }}>
@@ -44,15 +47,36 @@ const Sidebar = () => {
         align={align}
       >
         <Icon ImgSrc={MapIcon} text={"Mapa"} width={iconWidth} height={iconHeight} />
-        <Icon ImgSrc={WarningIcon} text={"Denuncias"} width={iconWidth} height={iconHeight} onClick={() => setShowWarningCard(!showWarningCard)} />
+        <Icon ImgSrc={WarningIcon} text={"Denuncias"} width={iconWidth} height={iconHeight} onClick={onWarningOpen} />
         <Spacer display={displaySpacer} />
-        <Icon ImgSrc={UserIcon} text={"Você"} width={iconWidth} height={iconHeight} />
+        <Icon ImgSrc={UserIcon} text={"Você"} width={iconWidth} height={iconHeight} onClick={onProfileOpen} />
       </SidebarBox>
-      {showWarningCard &&
-        <SidebarBox position="fixed" left={{ base: "0", md: "8%" }} bottom={{ base: "0", md: "0" }} width={{ base: "100%", md: "35%" }} height={{ base: "100%", md: "auto" }} zIndex={1}>
-          <WarningCard title="Netto precisa de ajuda" ImgSrc={Geleira} text="Ola amiguinhos. Gelo Gelo Gelo Gelo Gelo, Geleira Geleira Geleira. Escalada Escalada Escalada Escalada Escalada Escalala. " />
-        </SidebarBox>
-      }
+      {isWarningOpen && (
+        <Drawer placement="left" onClose={onWarningClose} isOpen={isWarningOpen} size={drawerSize}>
+          <DrawerOverlay>
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader>Netto precisa de ajuda</DrawerHeader>
+              <DrawerBody>
+                <WarningCard title="Netto precisa de ajuda" ImgSrc={Geleira} text="Ola amiguinhos. Gelo Gelo Gelo Gelo Gelo, Geleira Geleira Geleira. Escalada Escalada Escalada Escalada Escalada Escalala. " />
+              </DrawerBody>
+            </DrawerContent>
+          </DrawerOverlay>
+        </Drawer>
+      )}
+      {isProfileOpen && (
+        <Drawer placement="left" onClose={onProfileClose} isOpen={isProfileOpen} size={drawerSize}>
+          <DrawerOverlay>
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader>Profile</DrawerHeader>
+              <DrawerBody>
+                <ProfileCard title="User Profile" text="Here is some information about the user." />
+              </DrawerBody>
+            </DrawerContent>
+          </DrawerOverlay>
+        </Drawer>
+      )}
     </Flex>
   );
 };
