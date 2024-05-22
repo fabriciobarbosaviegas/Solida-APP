7f9fc8d41b9b96343acb9129fb413c63416fbad9
Marcus-Schebek <marcusschebek@gmail.com>
2024-05-22 18:19:37 -0300

    Correção na API para retornar o ID do usuário ao efetuar o login

--- api/Solida-API/app/controllers/auth_controller.ts
+++ api/Solida-API/app/controllers/auth_controller.ts
@@ -3,25 +3,25 @@ import User from '../models/user.js'
 import hash from '@adonisjs/core/services/hash'
 
 export default class AuthController {
-    async login({request}: HttpContext) {
-        try {
-            const user = await User.findByOrFail({
-                email: request.body().email,
-            })
+  async login({ request }: HttpContext) {
+    try {
+      const user = await User.findByOrFail({
+        email: request.body().email,
+      })
 
-            if(!(await hash.verify(user.password, request.body().password))) {
-                return "Wrong password/email"
-            }
+      if (!(await hash.verify(user.password, request.body().password))) {
+        return 'Wrong password/email'
+      }
 
-            const token = await User.accessTokens.create(user);
+      const token = await User.accessTokens.create(user)
 
-            return {
-                type: 'bearer',
-                value: token.value!.release(),
-            }
-
-        } catch(Error: any) {
-            return Error;
-        }
+      return {
+        type: 'bearer',
+        value: token.value!.release(),
+        userId: user.id,
+      }
+    } catch (Error: any) {
+      return Error
     }
+  }
 }
