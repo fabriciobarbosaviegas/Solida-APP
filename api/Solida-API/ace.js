b4604176aaf75158abeb33b9eeeb263e9551a11e
Marcus-Schebek <marcusschebek@gmail.com>
2024-05-26 01:47:17 -0300

    Revert "Mudança no ace.js para deploy"
    This reverts commit bde7f00b8e1ee6c5cee4b40ff31c90751a067aa0.

--- api/Solida-API/ace.js
+++ api/Solida-API/ace.js
@@ -20,7 +20,7 @@
  * Register hook to process TypeScript files using ts-node
  */
 import { register } from 'node:module'
-register('node/esm', import.meta.url)
+register('ts-node/esm', import.meta.url)
 
 /**
  * Import ace console entrypoint
